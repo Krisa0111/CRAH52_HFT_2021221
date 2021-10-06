634992e2844528648254829065d9b6e572fc3a36
Krisa0111 <pingipongi01@gmail.com>
2021-10-06 20:49:22 +0200

    mdf and ldf removed from gitignore

---
+++ CRAH52_HFT_2021221.Data/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace CRAH52_HFT_2021221.Data
+{
+    public class Class1
+    {
+    }
+}
---
+++ CRAH52_HFT_2021221.Logic/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace CRAH52_HFT_2021221.Logic
+{
+    public class Class1
+    {
+    }
+}
