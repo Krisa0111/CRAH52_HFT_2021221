5ee15094ad7519028e264dda8f164e708bda5019
Krisa0111 <pingipongi01@gmail.com>
2021-10-17 15:15:32 +0200

    Basic tests removed

--- CRAH52_HFT_2021221.Client/Program.cs
+++ CRAH52_HFT_2021221.Client/Program.cs
@@ -7,13 +7,7 @@ namespace CRAH52_HFT_2021221.Client
     {
         static void Main(string[] args)
         {
-            ClubsDbContext ctx = new ClubsDbContext();
-            ctx.SaveChanges();
-            foreach (var item in ctx.Events)
-            {
-                Console.WriteLine(item.EventName);
-            }
-            Console.ReadLine();
+            Console.WriteLine("Hello");
         }
     }
 }
