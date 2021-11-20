5808b297fdbe49130a5c20b1b7f79c28441ac1cb
Krisa0111 <pingipongi01@gmail.com>
2021-11-20 14:56:18 +0100

    ICLubsLogic methods created

--- CRAH52_HFT_2021221.Logic/Interfaces/IClubsLogic.cs
+++ CRAH52_HFT_2021221.Logic/Interfaces/IClubsLogic.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using CRAH52_HFT_2021221.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,5 +9,12 @@ namespace CRAH52_HFT_2021221.Logic
 {
     interface IClubsLogic
     {
+        void Create(Clubs club);
+        Clubs ReadOne(int id);
+        IQueryable<Clubs> ReadAll();
+        void Update(Clubs club);
+        void Delete(int id);
+        //NONCRUD
+        IEnumerable<Clubs> TheMostPopularCLub(); //within the number of events
     }
 }
