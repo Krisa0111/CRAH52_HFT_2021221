dd75e332ab5cdde27ef8b4f3d5191fab55963529
Krisa0111 <pingipongi01@gmail.com>
2021-11-20 19:58:51 +0100

    Little changes

--- CRAH52_HFT_2021221.Logic/Classes/EventsLogic.cs
+++ CRAH52_HFT_2021221.Logic/Classes/EventsLogic.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using CRAH52_HFT_2021221.Models;
+using CRAH52_HFT_2021221.Repository;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +8,53 @@ using System.Threading.Tasks;
 
 namespace CRAH52_HFT_2021221.Logic
 {
-    class EventsLogic
+    class EventsLogic : IEventsLogic
     {
+        IEventsRepository repo;
+        public void Create(Events events)
+        {
+            repo.Create(events);
+        }
+
+        public void Delete(int id)
+        {
+            repo.Delete(id);
+        }
+
+        public IEnumerable<Events> EventWithTheYoungestPerson()
+        {
+            //return repo.ReadAll()
+            //    .Select(x=>x)
+            //    .Where(y=>y.Guests)
+                throw new NotImplementedException();
+        }
+
+        public IEnumerable<Events> MostExpensiveEvents()
+        {
+            //return repo.ReadAll()
+            //    .Select(x=>x)
+            //    .Where(y=>y.Clubs.BaseTicketPrice)
+            throw new NotImplementedException();
+        }
+
+        public IQueryable<Events> ReadAll()
+        {
+            return repo.ReadAll();
+        }
+
+        public Events ReadOne(int id)
+        {
+            return repo.ReadOne(id);
+        }
+
+        public IEnumerable<Events> TheMostPopularEvent()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Update(Events events)
+        {
+            repo.Update(events);
+        }
     }
 }
--- CRAH52_HFT_2021221.Logic/Classes/GuestsLogic.cs
+++ CRAH52_HFT_2021221.Logic/Classes/GuestsLogic.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using CRAH52_HFT_2021221.Models;
+using CRAH52_HFT_2021221.Repository;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +8,32 @@ using System.Threading.Tasks;
 
 namespace CRAH52_HFT_2021221.Logic
 {
-    class GuestsLogic
+    class GuestsLogic : IGuestsLogic
     {
+        IGuestsRepository repo;
+        public void Create(Guests guest)
+        {
+            repo.Create(guest);
+        }
+
+        public void Delete(int id)
+        {
+            repo.Delete(id);
+        }
+
+        public IQueryable<Guests> ReadAll()
+        {
+            return repo.ReadAll();
+        }
+
+        public Guests ReadOne(int id)
+        {
+            return repo.ReadOne(id);
+        }
+
+        public void Update(Guests guest)
+        {
+            repo.Update(guest);
+        }
     }
 }
