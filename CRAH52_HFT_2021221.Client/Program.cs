1cc0c536955bec39b8ef6f72ee6ff2389b6d4156
Krisa0111 <pingipongi01@gmail.com>
2021-11-21 23:03:41 +0100

    Fixes

--- CRAH52_HFT_2021221.Client/Program.cs
+++ CRAH52_HFT_2021221.Client/Program.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using CRAH52_HFT_2021221.Data;
+using CRAH52_HFT_2021221.Logic; // SZEDD KI TE BAROM
+using CRAH52_HFT_2021221.Repository; // EZT IS
 
 namespace CRAH52_HFT_2021221.Client
 {
@@ -8,6 +10,7 @@ namespace CRAH52_HFT_2021221.Client
         static void Main(string[] args)
         {
             ClubsDbContext ctx = new ClubsDbContext();
+
             Console.WriteLine("Hello");
             Console.ReadLine();
         }
